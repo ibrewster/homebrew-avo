bdd9269bbf8889da4f979cfe8a771b99e957417a
Israel Brewster <israel@brewstersoft.com>
2020-08-03 09:57:18 -0800

    Update SHA for hourssince. Fixes #7

--- hourssince.rb
+++ hourssince.rb
@@ -2,8 +2,8 @@ class Hourssince < Formula
   desc "Functions and subroutines that calculate the number of hours of a given calendar date from Jan. 1 of a reference year"
   homepage "https://github.com/usgs/volcano-ash3d-hourssince"
   url "https://github.com/usgs/volcano-ash3d-hourssince/archive/master.tar.gz"
-  version "0.1"  # I had to make up a version number since there are no releases
-  sha256 "56e42abc1431239b1b772f8e6eee6d43386b114d2e00400548e9ff9a58dfcaf3"
+  version "0.2"  # I had to make up a version number since there are no releases
+  sha256 "2a4b1fb6e872ac49e45ccef7758186c8b78c64e71e58cc8dd23dea610ef43793"
 
   depends_on "gcc" => :build
 
