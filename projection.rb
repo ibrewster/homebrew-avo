46cf7ecd34ff7bb01ed1c42a5e8c3e3c39cc4a38
Israel Brewster <ijbrewster@alaska.edu>
2020-10-22 14:04:33 -0800

    Update projection formula to work with modified projection code

--- projection.rb
+++ projection.rb
@@ -1,14 +1,18 @@
 class Projection < Formula
   desc "Performs conversions between lat/lon and various projections commonly used in NWP forecast/reanalysis products"
   homepage "https://github.com/usgs/volcano-ash3d-projection"
-  url "https://github.com/usgs/volcano-ash3d-projection/archive/master.tar.gz?_=2"
-  version "0.2"  # I had to make up a version number since there are no releases
-  sha256 "49959024fea65be92c4a206bf9650ec3ec1045c1c50da6bc892f6e4040517bee"
+  url "https://github.com/ibrewster/volcano-ash3d-projection/archive/master.tar.gz?_=3"
+  version "0.3"  # I had to make up a version number since there are no releases
+  sha256 "173b0d4692001da97b3fd4da33d29c9e751f546f5eb65f1c466395457714905d"
+  revision 2
 
   depends_on "gcc" => :build
+  depends_on "autoconf"
 
   def install
-    system "make", "all", "FC=/usr/local/bin/gfortran"
-    system "make", "install", "INSTALLDIR=#{prefix}"
+    system "autoconf"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "all"
+    system "make", "install"
   end
 end
