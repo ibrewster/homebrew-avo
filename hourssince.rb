001b4fe70e12f9bd549e145f936077db3a4478dd
Israel Brewster <ijbrewster@alaska.edu>
2020-10-22 13:11:38 -0800

    Use forked version of hourssince with configure script
    Hopefully fixes #8

--- hourssince.rb
+++ hourssince.rb
@@ -1,14 +1,16 @@
 class Hourssince < Formula
   desc "Functions and subroutines that calculate the number of hours of a given calendar date from Jan. 1 of a reference year"
   homepage "https://github.com/usgs/volcano-ash3d-hourssince"
-  url "https://github.com/usgs/volcano-ash3d-hourssince/archive/master.tar.gz"
-  version "0.2"  # I had to make up a version number since there are no releases
-  sha256 "2a4b1fb6e872ac49e45ccef7758186c8b78c64e71e58cc8dd23dea610ef43793"
+  url "https://github.com/ibrewster/volcano-ash3d-hourssince/archive/master.tar.gz"
+  version "0.3"  # I had to make up a version number since there are no releases
+  sha256 "22ff5041468bc5404f33f41974e9f3fc48d51742366bedcd9f17d399d0b2a004"
 
   depends_on "gcc" => :build
 
   def install
-    system "make", "all", "FC=/usr/local/bin/gfortran"
-    system "make", "install", "INSTALLDIR=#{prefix}"
+    system "autoconf"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
   end
 end
