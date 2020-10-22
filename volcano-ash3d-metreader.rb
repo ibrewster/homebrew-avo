2eeccd95970c3a257e35898082e42a4113801fda
Israel Brewster <ijbrewster@alaska.edu>
2020-10-22 15:29:57 -0800

    Next attempt at making metreader build on linux

--- volcano-ash3d-metreader.rb
+++ volcano-ash3d-metreader.rb
@@ -1,15 +1,16 @@
 class VolcanoAsh3dMetreader < Formula
   desc "USGS Volcano Ash3d MetReader"
   homepage "https://github.com/ibrewster/volcano-ash3d-metreader"
-  url "https://github.com/ibrewster/volcano-ash3d-metreader/archive/master.zip?_=2_1"
-  version "0.2"
-  sha256 "6ce98e01efc00469321a16e120334c0cc9248a7322ceb82e25842e922de3f740"
-  revision 3
+  url "https://github.com/ibrewster/volcano-ash3d-metreader/archive/master.zip?_=3"
+  version "0.3"
+  sha256 "9e428d0a7fc4a0e431e288581c06230a63e9d1a0e7bc02b5185fcd6477af042a"
+  revision 1
 
   depends_on "gcc"
   depends_on "autoconf"
   depends_on "netcdf"
   depends_on "eccodes"
+  depends_on "ibrewster/avo/projection"
   depends_on "ibrewster/avo/hourssince"
   depends_on "ibrewster/avo/projection"
   
